de53b08e34c4a6d4c4cb6881ec98f57257a92cb3
joaocidralv <joaocidralv@gmail.com>
2023-08-23 09:26:38 -0300

    Criando o header

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/header";
+import styles from "./Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <Header />
+  </div>
+  );
+}
